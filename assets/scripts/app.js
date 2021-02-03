ff7569ac798fca8d3c5d7ea08b0a70338edeb47a
Maxim <limarevm123@gmail.com>
2021-02-03 19:05:08 +0300

    first commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,30 @@
-const carouselDomElement = document.querySelector('#pandaCarousel');
-console.log(carouselDomElement);
+const accordionExample = document.getElementById('accordionExample');
+console.log(accordionExample);
 
-const carousel = new bootstrap.Carousel(carouselDomElement, {
-      interval: 5000,
-      slide: false,
-      wrap: true
-})
+accordionExample.innerHTML = getFaqListMarkdown(faqData);
 
+function getFaqListMarkdown(faq) {
 
-function goToPrevSlide() {
-    carousel.prev();
+    // console.log(faq);
+
+    let resultMarkdown = '';
+
+    faq.forEach(faq => {
+        resultMarkdown += 
+        `<div class="accordion-item">
+                    <h2 class="accordion-header" id="${faq.heading}">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#${faq.collapse}" aria-expanded="true" aria-controls="${faq.collapse}">
+                    ${faq.question} 
+                    </button>
+                    </h2>
+            <div id="${faq.collapse}" class="accordion-collapse collapse show" aria-labelledby="${faq.heading}" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                <p>${faq.answer}</p>
+                </div>
+            </div>
+        </div>`;
+    })
+
+    return resultMarkdown;
 }
 
-function goToNextSlide() {
-    carousel.next();
-}
